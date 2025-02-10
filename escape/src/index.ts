c4765032e5d79a7b0f9f379ccde9245e5174f8ad
alanbal <37486532+alanbal@users.noreply.github.com>
2025-02-10 13:03:20 +0000

    config changes

--- escape/src/index.ts
+++ escape/src/index.ts
@@ -12,58 +12,65 @@ async function embed() {
         model: 'BAAI/bge-small-en-v1.5'
     });
 
-    console.log(output)    
+    console.log(output)
 }
 
-async function translate() {
-    const result = await inference.translation({
-        model: 't5-base',
-        inputs: 'How is the weather in Paris?'
-    })
-    console.log(result)
-}
+embed()
 
-async function translate2() {
-    const result = await inference.translation({
-        model: 'facebook/nllb-200-distilled-600M',
-        inputs: 'How is the weather in Paris?',
-        //@ts-ignore
-        parameters: {
-            src_lang: 'eng-Latn',
-            tgt_lang: 'spaa_Latn'
-        }
-    })
-    console.log(result)
-}
+// async function translate() {
+//     const result = await inference.translation({
+//         model: 't5-base',
+//         inputs: 'How is the weather in Paris?'
+//     })
+//     console.log(result)
+// }
 
-async function answerQuestion() {
-    const result = await inference.questionAnswering({
-        inputs: {
-            context: 'The quick brown fox jumps over the lazy dog',
-            question: 'What color is the fox?',
-            // question: 'Is the dog lazy?',
-            //question: 'What is the meaning of life?'
-        }
-    })
-    console.log(result);
-}
+// //translate()
+
+// async function translate2() {
+//     const result = await inference.translation({
+//         model: 'facebook/nllb-200-distilled-600M',
+//         inputs: 'How is the weather in Paris?',
+//         //@ts-ignore
+//         parameters: {
+//             src_lang: 'eng-Latn',
+//             tgt_lang: 'spaa_Latn'
+//         }
+//     })
+//     console.log(result)
+// }
+
+// //translate2()
+
+// async function answerQuestion() {
+//     const result = await inference.questionAnswering({
+//         inputs: {
+//             context: 'The quick brown fox jumps over the lazy dog',
+//             question: 'What color is the fox?',
+//             // question: 'Is the dog lazy?',
+//             //question: 'What is the meaning of life?'
+//         }
+//     })
+//     console.log(result);
+// }
+
+// //answerQuestion()
+
+// async function textToImage() {
+//     const result = await inference.textToImage({
+//         inputs: 'Cat in the hat on a mat',
+//         model: 'stabilityai/stable-diffusion-2',
+//         parameters: {
+//             negative_prompt: 'blurry'
+//         }
+//     });
+
+//     const buffer = Buffer.from(await result.arrayBuffer());
+//     writeFile('image.png', buffer, () => {
+//         console.log('image saved')
+//     })
 
 
-async function textToImage() {
-    const result = await inference.textToImage({
-        inputs: 'Cat in the hat on a mat',
-        model: 'stabilityai/stable-diffusion-2',
-        parameters: {
-            negative_prompt: 'blurry'
-        }
-    });
+// }
 
-    const buffer = Buffer.from(await result.arrayBuffer());
-    writeFile('image.png', buffer, ()=>{
-        console.log('image saved')
-    })
-
-    
-}
-
-textToImage()
+// textToImage()
